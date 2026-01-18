f9b1367888143c66877572c3a95400b1302882ce
suxar <suxar@EGOR>
2026-01-18 18:59:55 +0700

    ternary operator in set methods DONE

--- AbstractGeometry/main.cpp
+++ AbstractGeometry/main.cpp
@@ -1,56 +1,114 @@
-#include<Windows.H>
+#include<Windows.h>
 #include<iostream>
-
 using namespace std;
 
-enum Colour
+enum Color
 {
-	Red = 0x000000FF,
+	Black = 0x00000000,
+	Red = 0x000000FF,	
 	Green = 0x0000FF00,
 	Blue = 0x00FF0000,
 	Yellow = 0x0000FFFF,
 	Purple = 0x00800080,
+	White = 0x00FFFFFF
 };
 
+#define SHAPE_TAKE_PARAMETERS	int start_x, int start_y, int line_width, Color color
+#define SHAPE_GIVE_PARAMETERS	start_x, start_y, line_width, color
 class Shape
 {
-	Colour colour;
+	static const int MIN_START_X = 100;
+	static const int MIN_START_Y = 100;
+	static const int MAX_START_X = 1000;
+	static const int MAX_START_Y = 700;
+	static const int MIN_LINE_WIDTH = 1;
+	static const int MAX_LINE_WIDTH = 32;
+protected:
+	Color color;	
+	int start_x;
+	int start_y;
+	int line_width;
 public:
-	Shape(Colour colour) : colour(colour){}
+	int get_start_x()const
+	{
+		return start_x;
+	}
+	int get_start_y()const
+	{
+		return start_y;
+	}
+	int get_line_width()const
+	{
+		return line_width;
+	}
+	void set_start_x(int start_x)
+	{
+		start_x > MAX_START_X ? start_x = MAX_START_X : (start_x < MIN_START_X ? start_x = MIN_START_X : start_x = start_x);
+		//if (start_x < MIN_START_X)start_x = MIN_START_X;
+		//if (start_x > MAX_START_X)start_x = MAX_START_X;
+		this->start_x = start_x;
+	}
+	void set_start_y(int start_y)
+	{
+		start_y > MAX_START_Y ? start_y = MAX_START_Y : (start_y < MIN_START_Y ? start_y = MIN_START_Y : start_y = start_y);
+		//if (start_y < MIN_START_Y)start_y = MIN_START_Y;
+		//if (start_y > MAX_START_Y)start_y = MAX_START_Y;
+		this->start_y = start_y;
+	}
+	void set_line_width(int line_width)
+	{
+		line_width > MAX_LINE_WIDTH ? line_width = MAX_LINE_WIDTH : (line_width < MIN_LINE_WIDTH ? line_width = MIN_LINE_WIDTH : line_width = line_width);
+		//if (line_width < MIN_LINE_WIDTH)line_width = MIN_LINE_WIDTH;
+		//if (line_width > MAX_LINE_WIDTH)line_width = MAX_LINE_WIDTH;
+		this->line_width = line_width;
+	}
+	double filter_size(double size)
+	{
+		if (size < 20)size = 20;
+		if (size > 800)size = 500;
+		return size;
+	}
+	Shape(SHAPE_TAKE_PARAMETERS) :color(color)
+	{
+		set_start_x(start_x);
+		set_start_y(start_y);
+		set_line_width(line_width);
+	}
 	virtual ~Shape() {}
-	virtual double get_area() const = 0;
-	virtual double get_perimetr() const = 0;
+	virtual double get_area() const = 0;	
+	virtual double get_perimeter() const = 0;
 	virtual void draw() const = 0;
-	virtual void info()const
+	virtual void info() const
 	{
 		cout << "Площадь фигуры: " << get_area() << endl;
-		cout << "Площадь фигуры: " << get_perimetr() << endl;
+		cout << "Периметр фигуры: " << get_perimeter() << endl;
 		draw();
 	}
+
 };
 
-class Square:public Shape
+class Square :public Shape
 {
 	double side;
 public:
-	Square(double side, Colour colour) :Shape(colour)
+	Square(double side, SHAPE_TAKE_PARAMETERS) :Shape(SHAPE_GIVE_PARAMETERS)
 	{
-		this->side = side;
+		set_side(side);
 	}
-	~Square(){}
+	~Square() {}
 	void set_side(double side)
 	{
-		this->side = side;
+		this->side = filter_size(side);
 	}
 	double get_side()const
 	{
 		return side;
 	}
-	double get_area()const override
+	double get_area() const override
 	{
 		return side * side;
 	}
-	double get_perimetr()const override
+	double get_perimeter() const override
 	{
 		return side * 4;
 	}
@@ -65,28 +123,24 @@ public:
 			cout << endl;
 		}*/
 
-		HWND hwnd = GetConsoleWindow(); //1)Получаем окно консоли
-		HDC hdc = GetDC(hwnd); //получаем контекст окна консоли
-		//3)создаём чем и на чём мы будем рисовать
-		HPEN hPen = CreatePen(PS_SOLID, 5, Colour::Red);//контур фигуры
-		HBRUSH hBrush = CreateSolidBrush(Colour::Red); //заливка фигуры
+		HWND hwnd = GetConsoleWindow();	
+		HDC  hdc = GetDC(hwnd);
 
-		//вышесозданный инструмент нужно выбрать
+		HPEN hPen = CreatePen(PS_SOLID, line_width, color);
+		HBRUSH hBrush = CreateSolidBrush(color);
+
 		SelectObject(hdc, hPen);
 		SelectObject(hdc, hBrush);
-		
-		//Рисуем фигуру
-		Rectangle(hdc, 300, 300, 500, 500);
 
-		//удаляем инструменты для того что бы освободить ресурсы
+		Rectangle(hdc, start_x, start_y, start_x + side, start_y + side);
+		
 		DeleteObject(hBrush);
 		DeleteObject(hPen);
 		ReleaseDC(hwnd, hdc);
 	}
-
 	void info()const override
 	{
-		cout << typeid(*this).name() << endl;
+		cout << typeid(*this).name() + 6 << endl;
 		cout << "Длина стороны: " << get_side() << endl;
 		Shape::info();
 	}
@@ -95,12 +149,10 @@ public:
 void main()
 {
 	setlocale(LC_ALL, "");
-	//Shape shape = Colour::Red;
-
-	Square square(5, Colour::Red);
-	//cout << "Сторона квадрата: " << square.get_side() << endl;`
-	//cout << "Площадь фигуры: " << square.get_area() << endl;
-	//cout << "Площадь фигуры: " << square.get_perimetr() << endl;
-	//square.draw();
+	Square square(50000, -300, -300, 1, Color::White);
+	/*cout << "Сторона квадрата: " << square.get_side() << endl;
+	cout << "Площадь фигуры: " << square.get_area() << endl;
+	cout << "Периметр фигуры: " << square.get_perimeter() << endl;
+	square.draw();*/
 	square.info();
 }
